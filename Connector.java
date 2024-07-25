12c18953ed7fb5243c80f3efd3dabea58b1dbff5
juanpa0180 <juan.molinaro001@gmail.com>
2024-07-25 16:25:49 -0500

    Trabajo en progreso

--- Connector.java
+++ Connector.java
@@ -1,8 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connector {
         public static void connect() throws SQLException{
-            String url = "conexion";
+            String url = "conexion";//Cambia dependiendo de la BD que usemos
             String user = "root";
+            String password = "12345"; //Cambia dependiendo de la BD que usemos
+
+            Connection connection = DriverManager.getConnection(url, user, password);
+
+            System.out.println("Conexión exitosa a la BBDD");
+            connection.close();
         }
 }
