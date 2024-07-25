13cc420f2710b2b8d2df8d3d7e649f0499b44001
juanpa0180 <juan.molinaro001@gmail.com>
2024-07-25 12:30:03 -0500

    Borrado de JDBC y se reañadio como libreria. Inicio de clase Connector

--- Connector.java
+++ Connector.java
@@ -1,4 +1,8 @@
-package dev.lpa;
+import java.sql.SQLException;
 
 public class Connector {
+        public static void connect() throws SQLException{
+            String url = "conexion";
+            String user = "root";
+        }
 }
